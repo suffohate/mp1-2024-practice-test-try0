f366de37114e395fb934eb678850e174b3e325e6
suffohate <alexander145ru@gmail.com>
2024-12-05 10:03:39 +0300

    Update task1.c

--- 3824Б1ФИ3/18 Кулишов Александр Максимович/task1.c
+++ 3824Б1ФИ3/18 Кулишов Александр Максимович/task1.c
@@ -1,16 +1,13 @@
-// Задача 1. Найти сумму целых чисел a и b.
-// Если a + b >= INT_MAX, возвращать INT_MAX
-// Если a + b <= INT_MIN, возвращать INT_MIN
+#include <stdio.h>
 #include <limits.h>
 
 int sum_of_two_numbers(int a, int b) {
-    long long sum = (long long)a + (long long)b;
-  
-    if (sum >= INT_MAX) {
+
+    if (a > 0 && b > 0 && a > INT_MAX - b) {
         return INT_MAX;
-    } else if (sum <= INT_MIN) {
-        return INT_MIN;
-    } else {
-        return (int)sum;
     }
-}
+    if (a < 0 && b < 0 && a < INT_MIN - b) {
+        return INT_MIN;
+    }
+    return a + b;
+        }
